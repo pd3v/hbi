16a492a50a37e6ebd73c44316e33002cdb984af0
pd3v <paulo.develop@gmail.com>
2024-05-22 18:15:52 +0100

    delete commented code

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 
 	for (int i = 0; i < 20; i++) {
 		heyPhrase += "hey ";		
-   	std::cout << heyPhrase << " :" << /*(char)(110 + i)*/ i+1 << std::endl;
+   	std::cout << heyPhrase << " :" << i+1 << std::endl;
 	}
 
 	return 0;
